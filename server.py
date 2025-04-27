43d4b553aad30bcdd5977e01ab0e250888b1205b
Edward Wang <edawordwang@gmail.com>
2025-04-27 01:52:54 -0400

    works

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 
-ESP32_IP   = '192.168.1.42'  # replace with your ESP32’s IP
+ESP32_IP   = '192.168.1.25'  # replace with your ESP32’s IP
 ESP32_PORT = 5000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
